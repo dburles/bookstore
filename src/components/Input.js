cb6cbb3560d173414654e630abf3887d4bee5ab8
David Burles <dburles@gmail.com>
2018-12-20 20:17:55 +1100

    Update Input.js

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,10 +1,20 @@
 import styled from 'styled-components';
-import { space, width, fontSize } from 'styled-system';
+import {
+  space,
+  width,
+  fontSize,
+  border,
+  borderRadius,
+  borderColor,
+} from 'styled-system';
 
 const Input = styled.input`
   ${space}
   ${width}
   ${fontSize}
+  ${border}
+  ${borderColor}
+  ${borderRadius}
   height: 40px;
 `;
 
@@ -13,6 +23,9 @@ Input.defaultProps = {
   px: 3,
   width: 1,
   type: 'text',
+  border: '1px solid',
+  borderColor: 'grey.7',
+  borderRadius: 4,
 };
 
 export default Input;
